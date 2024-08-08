e700243fd081ecde9ede8e8acba3d794ae2709b8
Houssem Eddine Chibouni <153816533+zc-houssem@users.noreply.github.com>
2024-08-08 15:46:59 +0100

    feat(second template enhancements): - add smart breaks accoriding to contents - add picture integrations

--- src/common/pdf/services/pdf.service.ts
+++ src/common/pdf/services/pdf.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as ejs from 'ejs';
 import * as puppeteer from 'puppeteer';
+import * as cheerio from 'cheerio';
 import { join } from 'path';
 import { promises as fs } from 'fs';
 
@@ -18,20 +19,51 @@ export class PdfService {
     );
     const template = await fs.readFile(templatePath, 'utf8');
 
-    const html = ejs.render(template, data);
+    const imagePath = join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'assets',
+      'images',
+      'logo.png',
+    );
+    const imageBase64 = await this.imageToBase64(imagePath);
+
+    const html = ejs.render(template, { ...data, logo: imageBase64 });
     const browser = await puppeteer.launch({
       ignoreDefaultArgs: ['--disable-extensions'],
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     await page.setContent(html);
-    await page.addStyleTag({
-      path: join(__dirname, '..', '..', '..', 'assets', 'css', 'bootstrap.css'),
-    });
+    const stylesheets = await this.getStylesheets(html);
+    for (const stylesheet of stylesheets) {
+      await page.addStyleTag({
+        path: join(__dirname, '..', '..', '..', 'assets', 'css', stylesheet),
+      });
+    }
     const pdfBuffer = await page.pdf({ format: 'A4' });
 
     await browser.close();
 
     return pdfBuffer;
+  }
+
+  async getStylesheets(html: string) {
+    const $ = cheerio.load(html);
+    const stylesheets: string[] = [];
+    $('link[rel="stylesheet"]').each((i, elem) => {
+      const href = $(elem).attr('href');
+      if (href) {
+        stylesheets.push(href);
+      }
+    });
+    return stylesheets;
+  }
+
+  async imageToBase64(imagePath: string): Promise<string> {
+    const imageBuffer = await fs.readFile(imagePath);
+    return `data:image/${imagePath.split('.').pop()};base64,${imageBuffer.toString('base64')}`;
   }
 }
