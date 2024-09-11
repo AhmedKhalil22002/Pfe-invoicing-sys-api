1ccba713b2ae41e7613cc3be3ffa30c098cf4c02
Houssem Eddine Chibouni <153816533+zc-houssem@users.noreply.github.com>
2024-09-11 10:00:06 +0100

    feat(configuration for favorite currencies)

--- src/modules/currency/services/currency.service.ts
+++ src/modules/currency/services/currency.service.ts
@@ -3,10 +3,14 @@ import { CurrencyRepository } from '../repositories/repository/currency.reposito
 import { CurrencyEntity } from '../repositories/entities/currency.entity';
 import { CreateCurrencyDto } from '../dtos/currency.create.dto';
 import { CurrencyNotFoundException } from '../errors/currency.notfound.error';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CurrencyService {
-  constructor(private readonly currencyRepository: CurrencyRepository) {}
+  constructor(
+    private readonly currencyRepository: CurrencyRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   async findOneById(id: number): Promise<CurrencyEntity> {
     const currency = await this.currencyRepository.findOneById(id);
@@ -15,9 +19,29 @@ export class CurrencyService {
     }
     return currency;
   }
-
   async findAll(): Promise<CurrencyEntity[]> {
-    return this.currencyRepository.findAll();
+    const currencies = await this.currencyRepository.findAll();
+
+    // Get favorite currencies from config
+    const favCurrencies = this.configService.get<Record<string, string>>(
+      'app-preferences.currency',
+    );
+    const favoriteCurrencyCodes = Object.values(favCurrencies).filter(Boolean);
+
+    // Create a map for quick look-up of favorite currency codes
+    const favoriteCurrencyMap = new Set(favoriteCurrencyCodes);
+
+    // Sort currencies: favorites first, then the rest
+    const reorderedCurrencies = currencies.sort((a, b) => {
+      const isAFavorite = favoriteCurrencyMap.has(a.code);
+      const isBFavorite = favoriteCurrencyMap.has(b.code);
+
+      if (isAFavorite && !isBFavorite) return -1; // a is a favorite, b is not
+      if (!isAFavorite && isBFavorite) return 1; // b is a favorite, a is not
+      return 0; // Both are favorites or neither are favorites, maintain original order
+    });
+
+    return reorderedCurrencies;
   }
 
   async save(createCurrencyDto: CreateCurrencyDto): Promise<CurrencyEntity> {
