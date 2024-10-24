2b1bd5630b0b6e4c880d28751cd3729a53196ca6
Houssem Eddine Chibouni <153816533+zc-houssem@users.noreply.github.com>
2024-10-24 10:36:09 +0100

    feat(module/invoice/repositories/entity/*, module/invoice/repositories/repository/*, V1_7__invoice_init.sql) - create invoice entities - implement repositories for invoice entities - prepare invoice migration

---
+++ src/modules/invoice/enums/invoice-status.enum.ts
@@ -0,0 +1,6 @@
+export enum INVOICE_STATUS {
+  Nonexistent = 'invoice.status.non_existent',
+  Paid = 'invoice.status.paid',
+  Unpaid = 'invoice.status.unpaid',
+  Expired = 'invoice.status.expired',
+}
