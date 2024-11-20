6f11096ce2a7d073bb1cab977e9d9690847b16f2
Houssem Eddine Chibouni <153816533+zc-houssem@users.noreply.github.com>
2024-11-20 16:46:33 +0100

    feat(V1_10__tax_withholding_init) - seed tax_withholding data

---
+++ src/migrations/V1_10__tax_withholding_init.sql
@@ -0,0 +1,23 @@
+CREATE TABLE
+    IF NOT EXISTS `tax-withholding` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `label` varchar(255) DEFAULT NULL,
+        `rate` float DEFAULT NULL,
+        `createdAt` TIMESTAMP DEFAULT NOW (),
+        `updatedAt` TIMESTAMP DEFAULT NOW (),
+        `deletedAt` TIMESTAMP DEFAULT NULL,
+        `isDeletionRestricted` BOOLEAN DEFAULT FALSE,
+        PRIMARY KEY (`id`)
+    );
+
+INSERT INTO `tax-withholding` (`label`, `rate`)
+VALUES
+    ('Frais - Régime forfaitaire', 10),
+    ('Frais - Régime forfaitaire', 15),
+    ('Frais - Régime réel', 3),
+    ('Frais - Régime réel', 5),
+    ('Loyer - Frais spéciaux - Société exportation', 2.5),
+    ('Marché - Frais généraux', 1),
+    ('Marché - Frais généraux', 1.5),
+    ('Marché - Frais spéciaux - Société exportation', 0.5),
+    ('Revenus des comptes épargne spéciaux', 20);
