4ad3285019d6bdc4c703c083060cfef461812652
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-01-20 18:25:07 +0100

    feat(gateways) - secure socket by verifying jwt and disconnet client if not authenticated

--- src/common/gateways/events/events.gateway.ts
+++ src/common/gateways/events/events.gateway.ts
@@ -5,14 +5,17 @@ import {
   OnGatewayInit,
   WebSocketGateway,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
+import { Socket } from '../interface/socket.interface';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @WebSocketGateway({
   path: '/ws',
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   },
 })
 export class EventsGateway
@@ -21,18 +24,32 @@ export class EventsGateway
   @WebSocketServer()
   server: Server;
 
-  constructor() {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
   afterInit() {}
 
   async handleConnection(client: Socket, ...args: any[]) {
-    console.log('connected with args : ', args);
-    if (false) {
+    const token = client.handshake?.headers?.authorization?.split(' ')[1];
+
+    if (!token) client.disconnect(true);
+
+    try {
+      const decoded = this.jwtService.verify(token, {
+        secret: this.configService.get('app.jwtSecret', {
+          infer: true,
+        }),
+      });
+      client.user = decoded;
+    } catch (error) {
       client.disconnect(true);
     }
+    console.log('connected with args : ', args);
   }
 
   handleDisconnect(client: Socket): any {
-    // console.log(client);
+    console.log(client);
     console.log('disconnected');
   }
 }
