d7d97f3f1ff102a0db1e9468b9f7a1dd4e48fc2d
zc-houssem <houssemeddine.chibouni@zedneycreative.com>
2025-01-29 16:59:40 +0100

    feat(websocket rooms and events) - add websocket rooms and events

--- src/common/gateways/events/events.gateway.ts
+++ src/common/gateways/events/events.gateway.ts
@@ -4,11 +4,16 @@ import {
   OnGatewayDisconnect,
   OnGatewayInit,
   WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
-import { Socket } from '../interface/socket.interface';
+import { Server, Socket as IoSocket } from 'socket.io';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { SocketRoom } from '../enum/room.enum';
+import { Logger } from '@nestjs/common';
+import { randomBytes } from 'crypto';
 
 @WebSocketGateway({
   path: '/ws',
@@ -21,35 +26,111 @@ import { ConfigService } from '@nestjs/config';
 export class EventsGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
+  private readonly logger = new Logger(EventsGateway.name);
+
   @WebSocketServer()
   server: Server;
+
+  private rooms: Map<string, Set<string>> = new Map();
 
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
-  afterInit() {}
 
-  async handleConnection(client: Socket, ...args: any[]) {
-    const token = client.handshake?.headers?.authorization?.split(' ')[1];
-
-    if (!token) client.disconnect(true);
-
-    try {
-      const decoded = this.jwtService.verify(token, {
-        secret: this.configService.get('app.jwtSecret', {
-          infer: true,
-        }),
-      });
-      client.user = decoded;
-    } catch (error) {
-      client.disconnect(true);
-    }
-    console.log('connected with args : ', args);
+  afterInit() {
+    this.logger.log('WebSocket server initialized');
   }
 
-  handleDisconnect(client: Socket): any {
-    console.log(client);
-    console.log('disconnected');
+  async handleConnection(client: IoSocket) {
+    const token = client.handshake?.headers?.authorization?.split(' ')[1];
+    if (!token) {
+      client.disconnect(true);
+      return;
+    }
+    try {
+      const decoded = this.jwtService.verify(token, {
+        secret: this.configService.get('app.jwtSecret', { infer: true }),
+      });
+
+      if (!decoded || !decoded.email) {
+        client.disconnect(true);
+        return;
+      }
+
+      const uniqueId = randomBytes(4).toString('hex');
+      client.data.user = { ...decoded, email: `${decoded.email}#${uniqueId}` };
+
+      console.log(`Client connected: ${client.data.user.email}`);
+    } catch (error) {
+      client.disconnect(true);
+      console.error('Token verification failed:', error);
+      return;
+    }
+  }
+
+  @SubscribeMessage('joinRoom')
+  joinRoom(
+    @ConnectedSocket() client: IoSocket,
+    @MessageBody() roomName: SocketRoom,
+  ): void {
+    if (!client.data.user) {
+      client.disconnect(true);
+      return;
+    }
+    client.join(roomName);
+    if (!this.rooms.has(roomName)) {
+      this.rooms.set(roomName, new Set());
+    }
+    this.rooms.get(roomName)!.add(client.data.user.email);
+    console.log(`${client.data.user.email} joined room ${roomName}`);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  leaveRoom(
+    @ConnectedSocket() client: IoSocket,
+    @MessageBody() roomName: SocketRoom,
+  ): void {
+    if (!client.data.user) {
+      client.disconnect(true);
+      return;
+    }
+
+    client.leave(roomName);
+
+    // Remove user from the room, but keep the room alive
+    if (this.rooms.has(roomName)) {
+      this.rooms.get(roomName)!.delete(client.data.user.email);
+    }
+
+    console.log(`${client.data.user.email} left room ${roomName}`);
+  }
+
+  sendToRoom(roomName: SocketRoom, message: string, data: any): void {
+    if (this.rooms.has(roomName)) {
+      this.server.to(roomName).emit(message, data);
+      console.log(`Message sent to room ${roomName}: ${message}`);
+    } else {
+      console.log(`Room ${roomName} does not exist.`);
+    }
+  }
+
+  getRoomMembers(roomName: SocketRoom): string[] {
+    return this.rooms.has(roomName)
+      ? Array.from(this.rooms.get(roomName)!)
+      : [];
+  }
+
+  handleDisconnect(client: IoSocket): any {
+    if (!client.data.user || !client.data.user.email) {
+      console.log('Client disconnected, no user found');
+      return;
+    }
+
+    this.rooms.forEach((members) => {
+      members.delete(client.data.user.email);
+    });
+
+    console.log(`${client.data.user.email} disconnected`);
   }
 }
