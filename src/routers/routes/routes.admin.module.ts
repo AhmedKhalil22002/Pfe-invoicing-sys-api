019a5f7b0f53a032b5d83d199226f2db8320d112
softsurgery <109615722+softsurgery@users.noreply.github.com>
2025-08-22 15:50:37 +0100

    feat(store): add store module & adjust the controller into global routers

--- src/routers/routes/routes.admin.module.ts
+++ src/routers/routes/routes.admin.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { LoggerController } from 'src/shared/logger/controllers/logger.controller';
 import { LoggerModule } from 'src/shared/logger/logger.module';
+import { StoreController } from 'src/shared/store/controllers/store.controller';
+import { StoreModule } from 'src/shared/store/store.module';
 
 @Module({
-  controllers: [LoggerController],
+  controllers: [LoggerController, StoreController],
   providers: [],
   exports: [],
-  imports: [LoggerModule],
+  imports: [LoggerModule, StoreModule],
 })
 export class RoutesAdminModule {}
