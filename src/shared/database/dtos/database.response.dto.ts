7b4bdb6b85390af74e7671c14d9c6a6819721e1e
TheBackpacker1 <mohamedlouati002@gmail.com>
2025-08-28 12:41:17 +0100

    fix(Activitymodule):working to replace the messing async function

--- src/shared/database/dtos/database.response.dto.ts
+++ src/shared/database/dtos/database.response.dto.ts
@@ -4,7 +4,7 @@ import {
   DATABASE_CREATED_AT_FIELD_NAME,
   DATABASE_DELETED_AT_FIELD_NAME,
   DATABASE_UPDATED_AT_FIELD_NAME,
-} from 'src/common/database/constants/database.constant';
+} from 'src/shared/database/constants/database.constant';
 
 export abstract class ResponseDtoHelper {
   @ApiProperty({ type: Date })
