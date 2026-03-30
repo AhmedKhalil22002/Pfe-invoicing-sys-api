0ecf8f957f89924532f7177a2ca479f344176ae5
Ahmed Khalil <khalil.ahmed22002@gmail.com>
2026-03-30 12:36:50 +0100

    fix:fixing errors in bank-account

--- src/modules/bank-account/services/bank-account.service.ts
+++ src/modules/bank-account/services/bank-account.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { BankAccountRepository } from '../repositories/bank-account.repository';
 import { AbstractCrudService } from 'src/shared/database/services/abstract-crud.service';
 import { BankAccountEntity } from '../entities/bank-account.entity';
-import { BankAccountRepository } from '../repositories/bank-account.repository';
 
+@Injectable()
 export class BankAccountService extends AbstractCrudService<BankAccountEntity> {
   constructor(private readonly bankAccountRepository: BankAccountRepository) {
     super(bankAccountRepository);
